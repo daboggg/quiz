70215fd844eefa14001de8a269cbf18f5773e2e7
daboggg <vzinin@list.ru>
2023-04-22 17:57:31 +0300

    fix cors

--- quizback/quizback/settings.py
+++ quizback/quizback/settings.py
@@ -139,6 +139,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost",
     "http://127.0.0.1",
+    "http://localhost:9009",
+    "http://127.0.0.1:9009",
 ]
 
 CORS_ALLOW_METHODS = [
